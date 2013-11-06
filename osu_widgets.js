e42d76f0c09a3bc0eeae3b262b2e58a95517c11d
Brian Kildow <bkildow@gmail.com>
2013-11-06 09:13:04 -0500

    started slidshow work

---
+++ osu_widgets.js
@@ -0,0 +1,26 @@
+(function ($) {
+	Drupal.behaviors.osu_widgetsCarouselBehavior = {
+	  attach: function (context, settings) {
+	  	$('.bxcarousel', context).once('bxcarousel', function() {
+		  	$(this).bxSlider({
+			  minSlides: 2,
+			  maxSlides: 5,
+			  slideWidth: 250,
+			  slideMargin: 10,
+			  captions: true
+			});
+		});
+	  }
+	};
+
+	Drupal.behaviors.osu_widgetsSlideshowBehavior = {
+	  attach: function (context, settings) {
+	  	$('.bxslideshow', context).once('bxslideshow', function() {
+		  	$(this).bxSlider({
+				auto: true,
+  				autoControls: true
+			});
+		});
+	  }
+	};
+})(jQuery);
