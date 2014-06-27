638f57f2e2b0abb2cec703777cc9f3e0ca2be9a6
Brian Kildow <bkildow@gmail.com>
2014-06-27 11:07:31 -0400

    Added an osu spotlight fieldable panel pane

--- osu_widgets.js
+++ osu_widgets.js
@@ -1,30 +1,45 @@
 (function ($) {
-	Drupal.behaviors.osu_widgetsCarouselBehavior = {
-	  attach: function (context, settings) {
-	  	$('.bxcarousel', context).once('bxcarousel', function() {
-		  	$(this).bxSlider({
-			  minSlides: 2,
-			  maxSlides: 5,
-			  moveSlides: 1,
-			  slideWidth: 175,
-			  slideMargin: 36,
-			  captions: true,
-			  pager: false
-			});
-		});
-	  }
-	};
+  Drupal.behaviors.osu_widgetsCarouselBehavior = {
+    attach: function (context, settings) {
+      $('.bxcarousel', context).once('bxcarousel', function () {
+        $(this).bxSlider({
+          minSlides: 2,
+          maxSlides: 5,
+          moveSlides: 1,
+          slideWidth: 175,
+          slideMargin: 36,
+          captions: true,
+          pager: false
+        });
+      });
+    }
+  };
 
-	Drupal.behaviors.osu_widgetsSlideshowBehavior = {
-	  attach: function (context, settings) {
-	  	$('.bxslideshow', context).once('bxslideshow', function() {
-		  	$(this).bxSlider({
-				auto: true,
-				controls: false,
-				mode: 'fade',
-				pause: 7000
-			});
-		});
-	  }
-	};
+  Drupal.behaviors.osu_widgetsSlideshowBehavior = {
+    attach: function (context, settings) {
+      $('.bxslideshow', context).once('bxslideshow', function () {
+        $(this).bxSlider({
+          auto: true,
+          controls: false,
+          mode: 'fade',
+          pause: 7000
+        });
+      });
+    }
+  };
+
+  Drupal.behaviors.osu_widgetsSpotlightBehavior = {
+    attach: function (context, settings) {
+      $('.bxspotlight', context).once('bxspotlight', function () {
+        $(this).bxSlider({
+          auto: true,
+          controls: false,
+          mode: 'fade',
+          pause: 7000,
+          captions: true,
+          adaptiveHeight: true
+        });
+      });
+    }
+  };
 })(jQuery);
